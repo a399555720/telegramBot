a04e9a1cf74e8c0ea22ef7d8cb6753b574d3a943
Attiq <47444528+a399555720@users.noreply.github.com>
2021-04-26 21:29:08 +0800

    “First”

--- src/main/java/TronexPro.java
+++ src/main/java/TronexPro.java
@@ -39,88 +39,87 @@ public class TronexPro extends TelegramLongPollingBot {
             int g = 0;
             String c = "";
             String d = "";
-            long f = 40125940586562186L;
+            long f =40125940586562186L;
 
             System.out.println("start");
 
-//            while (true) {
-//                    try {
-//
-//                        String strURL = "https://bscscan.com/address/0x35806cB227F3a2fad27f0828C03608D6f9548A49";
-//                        URL url = new URL(strURL);
-//                        System.setProperty("http.agent", "Chrome");
-//                        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-//                        httpConn.setRequestProperty("Cookie", "foo=bar");
-//
-//                        InputStreamReader input = new InputStreamReader(httpConn.getInputStream(), "utf-8");
-//
-//                        BufferedReader bufReader = new BufferedReader(input);
-//                        String line = "";
-//                        StringBuilder contentBuf = new StringBuilder();
-//                        while ((line = bufReader.readLine()) != null) {
-//                            contentBuf.append(line);
-//                        }
-//                        String buf = contentBuf.toString();
-//                        int beginIx = buf.indexOf("Balance:", 1);
-//                        int endIx = buf.indexOf("BNB </div>", 1);
-//
-//                        String result = buf.substring(beginIx + 45 , endIx-10);
-//
-//
-//                        a = "0." + result;
-//                        System.out.println(a);
-//                        b = Float.parseFloat(a);
-//                        g++;
-//                        System.out.println(b + "--" + g);
-//                        Date currentTime = new Date();
-//                        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-//                        String dateString = formatter.format(currentTime);
-//                        System.out.println(dateString);
-//
-//
-////                        if (b > 0.04 ) {
-//                            System.out.println(a);
-//                            message.setText(a);
-//                            message.setChatId(update.getMessage().getChatId());
-//
-//                            try {
-//                                execute(message);
-//                            } catch (TelegramApiException e) {
-//                                e.printStackTrace();
-//                            }
-////                        }
-//
-//
-//                    } catch (Exception ex) {
-//                        ex.printStackTrace();
-//                    }
-//                try {
-//                    Thread.sleep(31000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
             while (true) {
-                message.setText("HI");
-                message.setChatId(update.getMessage().getChatId());
+                    try {
 
-                try {
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+                        String strURL = "https://bscscan.com/address/0x35806cB227F3a2fad27f0828C03608D6f9548A49";
+                        URL url = new URL(strURL);
+                        System.setProperty("http.agent", "Chrome");
+                        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                        httpConn.setRequestProperty("Cookie", "foo=bar");
 
+                        InputStreamReader input = new InputStreamReader(httpConn.getInputStream(), "utf-8");
+
+                        BufferedReader bufReader = new BufferedReader(input);
+                        String line = "";
+                        StringBuilder contentBuf = new StringBuilder();
+                        while ((line = bufReader.readLine()) != null) {
+                            contentBuf.append(line);
+                        }
+                        String buf = contentBuf.toString();
+                        int beginIx = buf.indexOf("Balance:", 1);
+                        int endIx = buf.indexOf("BNB </div>", 1);
+
+                        String result = buf.substring(beginIx + 45 , endIx-10);
+
+
+                        a = "0." + result;
+                        System.out.println(a);
+                        b = Float.parseFloat(a);
+                        g++;
+                        System.out.println(b + "--" + g);
+                        Date currentTime = new Date();
+                        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+                        String dateString = formatter.format(currentTime);
+                        System.out.println(dateString);
+
+
+                        if (b > 0.04 ) {
+                            System.out.println(a);
+                            message.setText(a);
+                            message.setChatId(update.getMessage().getChatId());
+
+                            try {
+                                execute(message);
+                            } catch (TelegramApiException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(31000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
+            }
+//            while (true) {
+//                message.setText("HI");
+//                message.setChatId(update.getMessage().getChatId());
+//
+//                try {
+//                    execute(message);
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
             }
 
         }
     }
-}
 
 
 
